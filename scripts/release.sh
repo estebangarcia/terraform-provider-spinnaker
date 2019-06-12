f6ba1adce53bdb97b2a5f47d790d763433fee6d9
Esteban Garcia <Esteban.Garcia@synchronoss.com>
2019-06-12 14:48:05 +0100

    update

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,4 +5,4 @@ tag=$1
 cd /go/src/stash.synchronoss.net/infrastructure/terraform-provider-spinnaker
 
 go get -u github.com/mitchellh/gox
-gox -os="linux darwin" -arch="amd64" -output="./artifacts/{{.OS}}_{{.Arch}}/terraform-provider-spinnaker-$tag" ./
+GO111MODULE=on gox -os="linux darwin" -arch="amd64" -output="./artifacts/{{.OS}}_{{.Arch}}/terraform-provider-spinnaker-$tag" ./
